730e4af7e0c6126d92f80d7a8ebb391aecb30ef5
ata <ataturkoglu@outlook.com.tr>
2021-09-11 18:41:36 +0300

    11.09.21

--- services/firebase.js
+++ services/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCmqgzJbmmdFMKkofQrjP7M4ZuLhiPptOw",
-    authDomain: "blogstore-99e7f.firebaseapp.com",
-    projectId: "blogstore-99e7f",
-    storageBucket: "blogstore-99e7f.appspot.com",
-    messagingSenderId: "661336382556",
-    appId: "1:661336382556:web:e66a298da99c8828e51a99",
-    measurementId: "G-H4JZM104XF"
+    
 };
 
 let app = null
